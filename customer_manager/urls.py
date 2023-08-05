8cbb689b4091fccc1479e5fba3f844b2c1bdc82a
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-08-04 21:04:16 -0500

    añadido urls de las apps

--- customer_manager/urls.py
+++ customer_manager/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalogo/', include('apps.catalogo_cuentas.urls')),
+    path('', include('apps.accounts.urls')),
+    path('user/', include('apps.user.urls')),
+    path('', include('apps.inicio.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
